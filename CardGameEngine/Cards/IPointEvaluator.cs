06c05a2f46e7eb96dd2c7a9876c2894441dab712
Anthony Kazyaka <ask.anthony@hotmail.com>
2021-03-20 01:12:35 -0400

    Remove unnecessary point evaluation logic in PointEvaluator

--- CardGameEngine/Cards/IPointEvaluator.cs
+++ CardGameEngine/Cards/IPointEvaluator.cs
@@ -4,8 +4,6 @@ namespace CardGameEngine.Cards
 {
     public interface IPointEvaluator
     {
-        bool IsValidCardForEvaluator(Card card);
-        int EvaluatePoints(Card card);
         int EvaluatePoints(List<Card> cards);
     }
 }
