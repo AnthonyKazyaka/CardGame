37aed71fbed0958d819b041bb643b5454d80cfe5
Anthony Kazyaka <ask.anthony@hotmail.com>
2021-03-20 02:27:44 -0400

    Remove unnecessary using statements

--- CardGameEngine/Decks/IDeckGenerator.cs
+++ CardGameEngine/Decks/IDeckGenerator.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CardGameEngine.Decks
+﻿namespace CardGameEngine.Decks
 {
     public interface IDeckGenerator
     {
