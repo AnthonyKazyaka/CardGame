f3eabd32bf20a483a0328be39704de2a1d15dd9a
Anthony Kazyaka <ask.anthony@hotmail.com>
2021-10-24 03:16:08 -0400

    Update point evaluation logic and location/namespace

---
+++ CardGameEngine/Game/PointEvaluators/IPointEvaluator.cs
@@ -0,0 +1,10 @@
+﻿using CardGameEngine.Cards;
+using System.Collections.Generic;
+
+namespace CardGameEngine.Game.PointEvaluators
+{
+    public interface IPointEvaluator
+    {
+        int EvaluatePoints(List<Card> cards);
+    }
+}
--- CardGameEngine/Players/Player.cs
+++ CardGameEngine/Players/Player.cs
@@ -5,6 +5,7 @@ namespace CardGameEngine.Players
 {
     public class Player
     {
+        public int Score { get; set; }
         public List<Card> Hand { get; set; } = new List<Card>();
     }
 }
